603d6f47c6af77aaead7ddb28f51744a8348206e
YunGibin <ggbin15@gmail.com>
2018-04-13 15:47:25 +0900

    1

---
+++ deque/deque/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim dq As Deque = New Deque()
+
+        dq.inserFront("A")
+        dq.print()
+
+        dq.inserFront("B")
+        dq.print()
+
+        dq.inserFront("C")
+        dq.print()
+
+        dq.insertRear("D")
+        dq.print()
+
+        dq.insertRear("E")
+        dq.print()
+
+        dq.insertRear("F")
+        dq.print()
+
+        Console.WriteLine("deledted :" + dq.deleteFront())
+        dq.print()
+
+        Console.WriteLine("deledted :" + dq.deleteRear())
+        dq.print()
+
+    End Sub
+
+End Module
